ea6503e512c85d84ae77dfaef8c6aa2315f73c9a
hearcomm <77430046+hearcomm@users.noreply.github.com>
2024-06-19 08:42:08 -0700

    Update offsetdict.py with Sihun device

--- offsetdict.py
+++ offsetdict.py
@@ -2,7 +2,8 @@ devices = [
  { 'mac': 'test', 'offset': 324 },
  { 'mac': 'test_avg', 'offset': 324 },
  { 'mac': '3', 'offset': 324 }
+ { 'mac' : '68:05:16:46:BF:24', 'offset': 384}
 ]
 
 mac_inst =[]
-mac_avg = ['test_avg']
+mac_avg = ['test_avg', '68:05:16:46:BF:24_avg']
