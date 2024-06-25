28e70c3f2e1ecc6e6786a99b5ce6e365e03775db
hearcomm <77430046+hearcomm@users.noreply.github.com>
2024-06-25 11:02:43 -0700

    Update offsetdict.py with avg mac

--- offsetdict.py
+++ offsetdict.py
@@ -1,9 +1,10 @@
 devices = [
  { 'mac': 'test', 'offset': 324 },
  { 'mac': 'test_avg', 'offset': 324 },
- { 'mac': '3', 'offset': 324 }
- { 'mac' : '68:05:16:46:BF:24', 'offset': 384}
-]
+ { 'mac': '3', 'offset': 324 },
+ { 'mac' : '68:05:16:46:BF:24', 'offset': 384}]
+
 
 mac_inst =[]
+
 mac_avg = ['test_avg', '68:05:16:46:BF:24_avg']
